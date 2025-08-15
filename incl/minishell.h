b448dd68ebd2fbbafe9af183c5ff1574780a54d2
Antti Pollanen <antti.pollanen89@gmail.com>
2025-08-15 15:51:39 +0300

    Added libraries to minishell.h

--- incl/minishell.h
+++ incl/minishell.h
@@ -12,7 +12,23 @@
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# include <curses.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
 # include <stdlib.h>
-# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <sys/types.h>
+# include <sys/resource.h>
+# include <term.h>
+# include <termios.h>
+# include <unistd.h>
+
 
 #endif
