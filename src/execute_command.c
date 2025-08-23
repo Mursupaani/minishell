201e8c582f41f7043c343bd2b5578afb2f92233e
Mursupaani <antti.pollanen89@gmail.com>
2025-08-23 11:56:36 +0300

    Added ft_split_arena and readline suppression

--- src/execute_command.c
+++ src/execute_command.c
@@ -13,25 +13,25 @@
 #include "minishell.h"
 
 // include char **envp inside cmd?
-int	execute_command(t_command cmd, char **envp)
+int	execute_command(t_command *cmd)
 {
-	cmd.pid = fork();
-	if (cmd.pid == 0)
+	cmd->pid = fork();
+	if (cmd->pid == 0)
 	{
-		if (execve(cmd.argv[0], cmd.argv, envp) == -1)
+		if (execve(cmd->argv[0], cmd->argv, cmd->envp) == -1)
 		{
 		// Replace this with similar to bash?
-			cmd.error = errno;
-			perror(strerror(cmd.error));
+			cmd->error = errno;
+			perror(strerror(cmd->error));
 		}
 	}
 	// How does the last parameter work?
-	if (waitpid(cmd.pid, &cmd.status, 0))
+	if (waitpid(cmd->pid, &cmd->status, 0))
 	{
 		// Replace this with proper error handling
-		cmd.error = errno;
-		perror(strerror(cmd.error));
+		cmd->error = errno;
+		perror(strerror(cmd->error));
 
 	}
-	return (cmd.status);
+	return (cmd->status);
 }
