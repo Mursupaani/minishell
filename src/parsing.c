a8f2b44e279729f0e7de69b33eb3a2f74eac8ab3
matusala3 <matusalakiflom@gmail.com>
2025-08-29 18:02:30 +0300

    continued on parsing

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: magebreh <magebreh@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 11:45:53 by anpollan          #+#    #+#             */
-/*   Updated: 2025/08/29 16:47:53 by magebreh         ###   ########.fr       */
+/*   Updated: 2025/08/29 17:58:26 by magebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,68 @@
 
 t_command	*parse_args(char *input, char **envp, t_arena *arena)
 {
-	t_command	*cmd;
-	char		**argv;
+    t_command	*cmd;
+    char		**argv;
 
-	cmd = (t_command *)arena_alloc(arena, sizeof(t_command));
-	if (!cmd)
-		printf("arena fail cmd\n");
-	// error handling
-	argv = ft_split_arena(input, ' ', arena);
-	if (!argv)
-		printf("arena fail argv\n");
-	cmd->argv = argv;
-	cmd->envp = envp;
-	return (cmd);
+    cmd = (t_command *)arena_alloc(arena, sizeof(t_command));
+    if (!cmd)
+        printf("arena fail cmd\n");
+    // error handling
+    argv = ft_split_arena(input, ' ', arena);
+    if (!argv)
+        printf("arena fail argv\n");
+    cmd->argv = argv;
+    cmd->envp = envp;
+    return (cmd);
 }
 
 t_command *parse_pipeline(t_token *tokens, t_shell *shell)
 {
-	t_command	*head;
-	t_command	*current;
-	t_command	*new_cmd;
-	t_token		*token;
+    t_command	*head;
+    t_command	*current;
+    t_command	*new_cmd;
+    t_token		*token;
 
-	head = NULL;
-	current = NULL;
-	token = tokens;
-	while(token)
-	{
-		if(token->type == TOKEN_PIPE)
-		{
-			new_cmd = create_command(shell->command_arena);
-			if(!new_cmd)
-				return (NULL);
-			if(!head)
-				head = new_cmd;
-			else
-				current->next = new_cmd;
-			current = new_cmd;
-			token = token->next;
-		}
-		else if(is_redir(token))
-		{
-			if(!current)
-			{
-				current = create_command(shell->command_arena);
-				if(!head)
-					head = current;
-			}
-			token = handle_redir(current, token, shell->command_arena);
-		}
-		else
-		{
-			if(!current)
-			{
-				current = create_command(shell->command_arena);
-				if(!head)
-					head = current;
-			}
-			add_word_to_command(current, token, shell->command_arena);
-			token = token->next;
-		}
-	}
-	return (head);
+    head = NULL;
+    current = NULL;
+    token = tokens;
+    while(token)
+    {
+        if(token->type == TOKEN_PIPE)
+        {
+            new_cmd = create_command(shell->command_arena);
+            if(!new_cmd)
+                return (NULL);
+            if(!head)
+                head = new_cmd;
+            else
+                current->next = new_cmd;
+            current = new_cmd;
+            token = token->next;
+        }
+        else if(is_redir(token))
+        {
+            if(!current)
+            {
+                current = create_command(shell->command_arena);
+                if(!head)
+                    head = current;
+            }
+            token = handle_redir(current, token, shell->command_arena);
+        }
+        else
+        {
+            if(!current)
+            {
+                current = create_command(shell->command_arena);
+                if(!head)
+                    head = current;
+            }
+            add_word_cmd(current, token, shell->command_arena);
+            token = token->next;
+        }
+    }
+    return (head);
 }
 
 t_command *create_command(t_arena *arena)
@@ -89,11 +89,11 @@ t_command *create_command(t_arena *arena)
     cmd->cmd_type = CMD_EXTERNAL;
     cmd->redirections = NULL;
     cmd->pipe_in[0] = -1;
-	cmd->pipe_in[1] = -1;
-	cmd->pipe_out[0] = -1;
-	cmd->pipe_out[1] = -1;
-	cmd->pid = -1;
-	cmd->next = NULL;
+    cmd->pipe_in[1] = -1;
+    cmd->pipe_out[0] = -1;
+    cmd->pipe_out[1] = -1;
+    cmd->pid = -1;
+    cmd->next = NULL;
     return (cmd);
 }
 
@@ -110,34 +110,59 @@ int is_redir(t_token *token)
 t_token *handle_redir(t_command *current, t_token *token, t_arena *arena)
 {
     t_redir *redir;
-	t_token *target;
-	t_redir	*tail;
-	
-	target = token->next;
-	redir = arena_alloc(arena, sizeof(t_redir));
-	if(!redir)
-		return (NULL);
-	if(target->type != TOKEN_WORD)
-		return (NULL);
-	redir = arena_alloc(arena, sizeof(t_redir));
-	if(!redir)
-		return (NULL);
-	redir->type = token_to_redir_type(token->type);
-	redir->target = arena_strdup(target->value, arena);
-	if(!redir->target)
-		return (NULL);
-	redir->fd = -1;
-	redir->next = NULL;
-	if(!current->redirections)
-		current->redirections = redir;
-	else
-	{
-		tail  = current->redirections;
-		while (tail->next)
-			tail = tail->next;
-		tail->next = redir;
-	}
-	return(target->next);
+    t_token *target;
+    t_redir	*tail;
+    
+    target = token->next;
+    redir = arena_alloc(arena, sizeof(t_redir));
+    if(!redir)
+        return (NULL);
+    if(target->type != TOKEN_WORD)
+        return (NULL);
+    redir = arena_alloc(arena, sizeof(t_redir));
+    if(!redir)
+        return (NULL);
+    redir->type = token_to_redir_type(token->type);
+    redir->target = arena_strdup(target->value, arena);
+    if(!redir->target)
+        return (NULL);
+    redir->fd = -1;
+    redir->next = NULL;
+    attach_redir(current, redir);
+}
+
+void add_word_cmd(t_command *cmd, t_token *word, t_arena *arena)
+{
+    int current_count;
+    int req_capacity;
+    char **new_argv;
+    int i;
+    
+    current_count = 0;
+    if(cmd->argv)
+    {
+        while (cmd->argv[current_count])
+            current_count++;
+    }
+    req_capacity = calculate_new_capacity(current_count);
+    if(!cmd->argv || needs_realloc(current_count))
+    {
+        new_argv = arena_alloc(arena, req_capacity*sizeof(char*));
+        if(!new_argv)
+            return ;
+        i = 0;
+        if(cmd->argv)
+        {
+            while (i < current_count)
+            {
+                new_argv[i] = cmd->argv[i];
+                i++;
+            }
+        }
+        cmd->argv = new_argv;
+    }
+    cmd->argv[current_count] = arena_strdup(word->value, arena);
+    cmd->argv[current_count + 1] = NULL;
 }
 
 t_redir_type token_to_redir_type(t_token_type token_type)
@@ -152,4 +177,30 @@ t_redir_type token_to_redir_type(t_token_type token_type)
         return (REDIR_HEREDOC);
     else
         return (REDIR_INPUT);
+}
+
+
+void attach_redir(t_command *cmd, t_redir *redir)
+{
+    if (!cmd->redirections)
+        cmd->redirections = redir;
+    else
+    {
+        t_redir *tail = cmd->redirections;
+        while (tail->next)
+            tail = tail->next;
+        tail->next = redir;
+    }
+}
+
+int calculate_new_capacity(int current_count)
+{
+    int  capacity;
+
+    if (current_count == 0)
+        return 8;
+    capacity = 8;
+    while (capacity <= current_count)
+        capacity *= 2;
+    return capacity;
 }
