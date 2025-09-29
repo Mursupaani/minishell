cd422201a1a407f2f01a7862020f220308090805
Antti Pollanen <antti.pollanen89@gmail.com>
2025-09-29 20:03:09 +0300

    Removed unnecessary file

--- src/execute_external_command.c
+++ src/execute_external_command.c
@@ -1,18 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   find_file_from_path.c                              :+:      :+:    :+:   */
+/*   execute_external_command.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpollan <anpollan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/10 13:35:19 by anpollan          #+#    #+#             */
-/*   Updated: 2025/09/11 12:13:14 by anpollan         ###   ########.fr       */
+/*   Created: 2025/09/29 17:10:23 by anpollan          #+#    #+#             */
+/*   Updated: 2025/09/29 17:23:02 by anpollan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static char	*try_paths(char *filename, char **path_dirs, t_shell *shell);
+static bool	check_file_type_and_permissions(char *filepath);
+
+void	execute_external_command(t_command *cmd, t_shell *shell)
+{
+	char	*executable_path;
+
+	if (cmd->argv[0][0] == '.' || cmd->argv[0][0] == '/')
+		executable_path = cmd->argv[0];
+	else
+		executable_path = find_file_from_path(cmd->argv[0], shell);
+	if (!executable_path)
+		return ;
+	if (cmd->redirections)
+		execute_redirection(cmd->redirections, cmd, shell);
+	if (!check_file_type_and_permissions(executable_path))
+	if (execve(executable_path, cmd->argv, shell->env_array))
+	{
+		shell->last_exit_status = 127;
+		ft_fprintf(STDERR_FILENO,
+			"minishell: %s: %s\n", strerror(errno), cmd->argv[0]);
+	}
+}
 
 char	*find_file_from_path(char *filename, t_shell *shell)
 {
@@ -41,5 +63,14 @@ static char	*try_paths(char *filename, char **path_dirs, t_shell *shell)
 		path_dirs++;
 	}
 	ft_fprintf(STDERR_FILENO, "%s: command not found\n", filename);
+	shell->last_exit_status = 127;
 	return (NULL);
+}
+
+static bool	check_file_type_and_permissions(char *filepath)
+{
+	if (!filepath)
+		return (false);
+
+	return (true);
 }
