73df8fd7e371bd8daaf1dc82b5cfaf124e1aa9a6
Antti Pollanen <antti.pollanen89@gmail.com>
2025-10-22 13:04:22 +0300

    Fixing sigint exit code

--- src/executor/execute_command.c
+++ src/executor/execute_command.c
@@ -96,12 +96,12 @@ static void	execute_single_external_command(t_command *cmd, t_shell *shell)
 	}
 	setup_execution_signals();
 	waitpid(shell->child_pid, &shell->last_exit_status, 0);
-	g_signal_received = 0;
-	setup_parent_signals();
 	if (WIFEXITED(shell->last_exit_status))
 		shell->last_exit_status = WEXITSTATUS(shell->last_exit_status);
 	else if (WIFSIGNALED(shell->last_exit_status))
 		shell->last_exit_status = 128 + WTERMSIG(shell->last_exit_status);
 	else
 		shell->last_exit_status = 1;
+	g_signal_received = 0;
+	setup_parent_signals();
 }
